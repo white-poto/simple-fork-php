08a5cb1fd9970ac083d8666fbb698afda8843063
huyanping <hypxm@qq.com>
2015-08-12 19:57:40 +0800

    add pool test

---
+++ tests/pool.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jenner
+ * Date: 2015/8/12
+ * Time: 19:55
+ */
+
+
+require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+class TestRunnable extends \Jenner\SimpleFork\Runnable{
+
+    /**
+     * 进程执行入口
+     * @return mixed
+     */
+    public function run()
+    {
+        sleep(10);
+        echo getmypid() . ':done' . PHP_EOL;
+    }
+}
+
+$pool = new \Jenner\SimpleFork\Pool();
+$pool->submit(new \Jenner\SimpleFork\Process(new TestRunnable()));
+$pool->submit(new \Jenner\SimpleFork\Process(new TestRunnable()));
+$pool->submit(new \Jenner\SimpleFork\Process(new TestRunnable()));
+
+$pool->start();
+$pool->wait();
