b5f7abf183cf2bbcd124d79dd820d0bd09b8bd3c
huyanping <hypxm@qq.com>
2015-08-12 20:41:11 +0800

    add shared memory test

---
+++ tests/pool_nowait.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jenner
+ * Date: 2015/8/12
+ * Time: 20:38
+ */
+
+require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+class TestRunnable extends \Jenner\SimpleFork\Runnable{
+
+    /**
+     * 进程执行入口
+     * @return mixed
+     */
+    public function run()
+    {
+        sleep(10);
+        echo getmypid() . ':done' . PHP_EOL;
+    }
+}
+
+$pool = new \Jenner\SimpleFork\Pool();
+$pool->submit(new \Jenner\SimpleFork\Process(new TestRunnable()));
+$pool->submit(new \Jenner\SimpleFork\Process(new TestRunnable()));
+$pool->submit(new \Jenner\SimpleFork\Process(new TestRunnable()));
+
+$pool->start();
+
+
+while($pool->aliveCount() > 0){
+    echo "i am waiting" . PHP_EOL;
+    $pool->wait(false);
+    sleep(1);
+}
