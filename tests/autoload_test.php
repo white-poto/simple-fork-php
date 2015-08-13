a3e4bef588aa1568549631743e5f7a76085716a6
huyanping <hypxm@qq.com>
2015-08-13 11:33:01 +0800

    add autoload.php and tests

--- tests/autoload_test.php
+++ tests/autoload_test.php
@@ -6,14 +6,17 @@
  * Time: 11:28
  */
 
+error_reporting(E_ALL);
+
 spl_autoload_register(function ($classname) {
     echo $classname . PHP_EOL;
     $dir = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR;
     echo $dir . PHP_EOL;
     if (stristr($classname, "\\Jenner\\SimpleFork\\") == 0) {
         $file = $dir . basename($classname);
+        echo basename($classname) . PHP_EOL;
         echo $file . PHP_EOL;
-        if (file_exists($file)) include $file;
+        if (file_exists($file)) require $file;
     }
 });
 
