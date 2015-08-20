894ed151de7524e252d9d7cd3a91a3b2198c13d7
huyanping <hypxm@qq.com>
2015-08-20 14:37:59 +0800

    update readme.md

---
+++ examples/before_start.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jenner
+ * Date: 2015/8/20
+ * Time: 14:37
+ */
+
+declare(ticks=1);
+require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+class TestRunnable extends \Jenner\SimpleFork\Runnable
+{
+
+    /**
+     * 进程执行入口
+     * @return mixed
+     */
+    public function run()
+    {
+        while (true) {
+            echo "I am running" . PHP_EOL;
+            sleep(1);
+        }
+    }
+
+}
+
+$process = new \Jenner\SimpleFork\Process(new TestRunnable());
+$process->on(\Jenner\SimpleFork\Execution::BEFORE_START, function(){
+    echo "start" . PHP_EOL;
+    return true;
+});
+
+$process->start();
+sleep(5);
+$process->stop();
