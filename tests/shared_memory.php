a3ba3c658d0907f2856b77e6e82c81a88850aa50
huyanping <hypxm@qq.com>
2015-10-23 17:39:01 +0800

    tests

---
+++ tests/shared_memory.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jenner
+ * Date: 2015/10/23
+ * Time: 17:38
+ */
+
+require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+$cache = new \Jenner\SimpleFork\Cache\SharedMemory();
+$cache->set('test', 'test');
+
+$cache->remove();
+var_dump($cache->get('test'));
