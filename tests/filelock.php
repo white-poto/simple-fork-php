47af8b1c87d6c8b273040338063b126238812f3e
huyanping <hypxm@qq.com>
2015-10-26 15:50:57 +0800

    tests

---
+++ tests/filelock.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jenner
+ * Date: 2015/10/26
+ * Time: 15:50
+ */
+
+require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+$lock_file = "/tmp/simple-fork.lock";
+if(!file_exists($lock_file)){
+    touch($lock_file);
+}
+$process = new \Jenner\SimpleFork\Process(function() use($lock_file){
+    $lock = \Jenner\SimpleFork\Lock\FileLock::create($lock_file);
+    echo getmypid() . PHP_EOL;
+    var_dump($lock->acquire());
+    sleep(5);
+    echo getmypid() . PHP_EOL;
+    var_dump($lock->release());
+});
+$process->start();
+sleep(3);
+$lock = \Jenner\SimpleFork\Lock\FileLock::create($lock_file);
+echo getmypid() . PHP_EOL;
+var_dump($lock->acquire());
+$process->wait();
+echo getmypid() . PHP_EOL;
+var_dump($lock->acquire());
+echo getmypid() . PHP_EOL;
+var_dump($lock->release());
---
+++ tests/semaphore.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jenner
+ * Date: 2015/10/26
+ * Time: 15:49
+ */
+require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+$process = new \Jenner\SimpleFork\Process(function(){
+    $lock = \Jenner\SimpleFork\Lock\Semaphore::create('test');
+    var_dump($lock->acquire());
+    sleep(5);
+    var_dump($lock->release());
+});
+$process->start();
+sleep(3);
+$lock = \Jenner\SimpleFork\Lock\Semaphore::create("test");
+$this->assertFalse($lock->acquire());
+$process->wait();
+$this->assertTrue($lock->acquire());
+$this->assertTrue($lock->release());
