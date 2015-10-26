4a3ea253a4731a5031770abaf9c5a7b93c19f3cd
huyanping <hypxm@qq.com>
2015-10-26 14:24:50 +0800

    tests

--- tests/PoolTest.php
+++ tests/PoolTest.php
@@ -12,7 +12,7 @@ class PoolTest extends PHPUnit_Framework_TestCase
         $pool = new \Jenner\SimpleFork\Pool();
         for($i=0; $i<10; $i++){
             $process = new \Jenner\SimpleFork\Process(function(){
-                sleep(10);
+                sleep(3);
             });
             $pool->submit($process);
         }
