bc634e2972707a4ed4dcbb5b51149781be480985
huyanping <hypxm@qq.com>
2015-10-26 17:59:27 +0800

    add default prefix

--- tests/SystemVMessageQueueTest.php
+++ tests/SystemVMessageQueueTest.php
@@ -22,11 +22,9 @@ class SystemVMessageQueueTest extends PHPUnit_Framework_TestCase
         $process = new \Jenner\SimpleFork\Process(function () {
             $queue = new \Jenner\SimpleFork\Queue\SystemVMessageQueue();
             $queue->put(1, 'test');
-            echo 'put' . PHP_EOL;
         });
         $process->start();
         $process->wait();
-        unset($process);
         $queue = new \Jenner\SimpleFork\Queue\SystemVMessageQueue();
         $this->assertEquals($queue->size(1), 1);
         $this->assertEquals($queue->get(1), 'test');
