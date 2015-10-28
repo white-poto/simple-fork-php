b319fd8ed43a3d38ada588ceeb038f9be36262df
huyanping <hypxm@qq.com>
2015-10-28 14:54:26 +0800

    fix bug

--- src/Process.php
+++ src/Process.php
@@ -340,6 +340,7 @@ class Process
         } else {
             $res = pcntl_waitpid($this->pid, $status, WNOHANG | WUNTRACED);
         }
+        var_dump($res);
 
         if ($res === -1) {
             $message = "pcntl_waitpid failed. the process maybe available";
