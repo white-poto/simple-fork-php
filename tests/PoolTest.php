fa0258c47e319773dd968593e902207601b55792
huyanping <hypxm@qq.com>
2015-10-28 15:16:56 +0800

    remove reload methods

--- tests/PoolTest.php
+++ tests/PoolTest.php
@@ -21,4 +21,38 @@ class PoolTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(10, $pool->aliveCount());
         $pool->wait();
     }
+
+    public function testShutdown()
+    {
+        $pool = new \Jenner\SimpleFork\Pool();
+        for ($i = 0; $i < 10; $i++) {
+            $process = new \Jenner\SimpleFork\Process(function () {
+                sleep(3);
+            });
+            $pool->submit($process);
+        }
+        $start = time();
+        $pool->start();
+        $pool->shutdown();
+        $time = time() - $start;
+        $this->assertTrue($time >= 3);
+        $this->assertEquals(0, $pool->aliveCount());
+    }
+
+    public function testShutdownForce()
+    {
+        $pool = new \Jenner\SimpleFork\Pool();
+        for ($i = 0; $i < 10; $i++) {
+            $process = new \Jenner\SimpleFork\Process(function () {
+                sleep(3);
+            });
+            $pool->submit($process);
+        }
+        $start = time();
+        $pool->start();
+        $pool->shutdownForce();
+        $time = time() - $start;
+        $this->assertTrue($time < 3);
+        $this->assertEquals(0, $pool->aliveCount());
+    }
 }
