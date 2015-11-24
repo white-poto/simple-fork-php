a46d17e76fba53e288d13c02f5f935666524d50c
huyanping <hypxm@qq.com>
2015-11-24 17:09:47 +0800

    add Pipe

---
+++ src/Pipe.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * @author Jenner <hypxm@qq.com>
+ * @blog http://www.huyanping.cn
+ * @license https://opensource.org/licenses/MIT MIT
+ * @datetime: 2015/11/24 16:29
+ */
+
+namespace Jenner\SimpleFork;
+
+
+class Pipe
+{
+    /**
+     * @var resource
+     */
+    protected $read;
+
+    /**
+     * @var resource
+     */
+    protected $write;
+
+    /**
+     * @var string
+     */
+    protected $filename;
+
+    /**
+     * @var bool
+     */
+    protected $block;
+
+    /**
+     * @param string $filename fifo filename
+     * @param int $mode
+     * @param bool $block if blocking
+     */
+    public function __construct($filename = '/tmp/simple-fork.pipe', $mode = 0666, $block = false)
+    {
+        if (!file_exists($filename) && !posix_mkfifo($filename, $mode)) {
+            throw new \RuntimeException("create pipe failed");
+        }
+        if (filetype($filename) != "fifo") {
+            throw new \RuntimeException("file exists and it is not a fifo file");
+        }
+
+        $this->filename = $filename;
+        $this->block = $block;
+    }
+
+    public function setBlock($block = true)
+    {
+        if (is_resource($this->read)) {
+            $set = stream_set_blocking($this->read, $block);
+            if (!$set) {
+                throw new \RuntimeException("stream_set_blocking failed");
+            }
+        }
+
+        if (is_resource($this->write)) {
+            $set = stream_set_blocking($this->write, $block);
+            if (!$set) {
+                throw new \RuntimeException("stream_set_blocking failed");
+            }
+        }
+
+        $this->block = $block;
+    }
+
+    /**
+     * @param int $size
+     * @return string
+     */
+    public function read($size = 1024)
+    {
+        if (!is_resource($this->read)) {
+            $this->read = fopen($this->filename, 'r+');
+            if (!is_resource($this->read)) {
+                throw new \RuntimeException("open file failed");
+            }
+            if (!$this->block) {
+                $set = stream_set_blocking($this->read, false);
+                if (!$set) {
+                    throw new \RuntimeException("stream_set_blocking failed");
+                }
+            }
+        }
+
+        return fread($this->read, $size);
+    }
+
+    /**
+     * @param $message
+     * @return int
+     */
+    public function write($message)
+    {
+        if (!is_resource($this->write)) {
+            $this->write = fopen($this->filename, 'w+');
+            if (!is_resource($this->write)) {
+                throw new \RuntimeException("open file failed");
+            }
+            if (!$this->block) {
+                $set = stream_set_blocking($this->write, false);
+                if (!$set) {
+                    throw new \RuntimeException("stream_set_blocking failed");
+                }
+            }
+        }
+
+        return fwrite($this->write, $message);
+    }
+
+    /**
+     *
+     */
+    public function close()
+    {
+        if (is_resource($this->read)) {
+            fclose($this->read);
+        }
+        if (is_resource($this->write)) {
+            fclose($this->write);
+        }
+    }
+
+    /**
+     *
+     */
+    public function __destruct()
+    {
+        $this->close();
+    }
+
+    public function remove()
+    {
+        return unlink($this->filename);
+    }
+}
---
+++ tests/PipeTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * @author Jenner <hypxm@qq.com>
+ * @blog http://www.huyanping.cn
+ * @license https://opensource.org/licenses/MIT MIT
+ * @datetime: 2015/11/24 17:00
+ */
+class PipeTest extends PHPUnit_Framework_TestCase
+{
+    public function testRead()
+    {
+        $process = new \Jenner\SimpleFork\Process(function () {
+            $pipe = new \Jenner\SimpleFork\Pipe();
+            $pipe->write('test');
+            sleep(2);
+            $pipe->close();
+        });
+        $process->start();
+        $pipe = new \Jenner\SimpleFork\Pipe();
+        $this->assertEquals('test', $pipe->read());
+        $process->wait(true);
+        $pipe->close();
+    }
+
+    public function testWrite()
+    {
+        $pipe = new \Jenner\SimpleFork\Pipe();
+        $this->assertEquals(4, $pipe->write('test'));
+
+        $process = new \Jenner\SimpleFork\Process(function () {
+            $pipe = new \Jenner\SimpleFork\Pipe();
+            $pipe->read();
+            $pipe->close();
+        });
+        $process->start();
+        $process->wait(true);
+        $pipe->close();
+    }
+
+    public function testBlock()
+    {
+        $pipe = new \Jenner\SimpleFork\Pipe();
+        $pipe->setBlock(true);
+        $process = new \Jenner\SimpleFork\Process(function () {
+            $pipe = new \Jenner\SimpleFork\Pipe();
+            sleep(2);
+            $pipe->write('test');
+            $pipe->close();
+        });
+        $start = time();
+        $process->start();
+        $this->assertEquals('test', $pipe->read());
+        $end = time();
+        $this->assertTrue(($end - $start) >= 2);
+    }
+}
