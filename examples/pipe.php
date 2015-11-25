5ae90e4bac45dbbe314478784a47cfd122ee8cb8
huyanping <hypxm@qq.com>
2015-11-25 11:09:20 +0800

    add autoload.php and tests

---
+++ examples/pipe.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author Jenner <hypxm@qq.com>
+ * @blog http://www.huyanping.cn
+ * @license https://opensource.org/licenses/MIT MIT
+ * @datetime: 2015/11/25 11:07
+ */
+
+require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+
+$pid = pcntl_fork();
+
+if($pid == 0){
+    $pipe = new \Jenner\SimpleFork\Queue\Pipe();
+    sleep(1);
+    $pipe->write("test");
+}else{
+    $pipe = new \Jenner\SimpleFork\Queue\Pipe();
+    $pipe->setBlock(true);
+    $result = $pipe->read();
+    echo $result . PHP_EOL;
+}
