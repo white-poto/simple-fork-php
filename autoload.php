a4cea568169559c3a15249c9d38a58fede97b29a
huyanping <hypxm@qq.com>
2015-11-25 10:56:17 +0800

    add autoload.php

---
+++ autoload.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Jenner <hypxm@qq.com>
+ * @blog http://www.huyanping.cn
+ * @license https://opensource.org/licenses/MIT MIT
+ * @datetime: 2015/11/25 10:55
+ */
+
+spl_autoload_register(function ($classname) {
+    $dir = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR;
+    if (stristr($classname, "\\Jenner\\SimpleFork\\") == 0) {
+        $file = $dir . basename(str_replace('\\', '/', $classname));
+        if (file_exists($file)) require $file . '.php';
+    }
+});
