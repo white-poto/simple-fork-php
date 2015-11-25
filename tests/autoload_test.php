7010a6d48ce2a7d9909a78d3271e2af5317f2cb0
huyanping <hypxm@qq.com>
2015-11-25 10:59:34 +0800

    add autoload.php and tests

--- tests/autoload_test.php
+++ tests/autoload_test.php
@@ -8,6 +8,6 @@
 
 error_reporting(E_ALL);
 
-require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'autoload.php';
+echo dirname(__DIR__) . DIRECTORY_SEPARATOR . 'autoload.php';
 
 var_dump(class_exists('\\Jenner\\SimpleFork\\Process'));
