70c051e27794d51410fa26fb4a083d7eb288c378
huyanping <hypxm@qq.com>
2016-02-01 09:27:59 +0800

    remove start when the process is already started in a pool

--- src/FixedPool.php
+++ src/FixedPool.php
@@ -33,7 +33,7 @@ class FixedPool extends AbstractPool
     {
         Utils::checkOverwriteRunMethod(get_class($process));
 
-        if ($this->aliveCount() < $this->max) {
+        if ($this->aliveCount() < $this->max && !$process->isStarted()) {
             $process->start();
         }
         array_push($this->processes, $process);
