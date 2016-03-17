a5ca2fd63cb01d87db674af3a59a599b0925a156
Jenner <hypxm@qq.com>
2016-03-17 20:13:06 +0800

    add redis-queue

--- examples/redis-queue/Consumer.php
+++ examples/redis-queue/Consumer.php
@@ -13,7 +13,7 @@ class Worker extends \Jenner\SimpleFork\Process
 {
     public function run()
     {
-        $queue = new \Jenner\SimpleFork\Queue\RedisQueue('127.0.0.1', 6379);
+        $queue = new \Jenner\SimpleFork\Queue\RedisQueue('127.0.0.1', 6379, 1);
         while (true) {
             $res = $queue->get();
             if ($res !== false) {
