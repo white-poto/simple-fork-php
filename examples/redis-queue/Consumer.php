ca409ec28eab30bd65eb5a66cf6f20db59dea4e7
Jenner <hypxm@qq.com>
2016-03-17 20:01:11 +0800

    add redis-queue

---
+++ examples/redis-queue/Consumer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jenner
+ * Date: 2016/3/17
+ * Time: 19:57
+ */
+
+declare(ticks = 1);
+require dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+class Worker extends \Jenner\SimpleFork\Process
+{
+    public function run()
+    {
+        $queue = new \Jenner\SimpleFork\Queue\RedisQueue('127.0.0.1', 6379);
+        while (true) {
+            $res = $queue->get();
+            if ($res !== false) {
+                echo $res . PHP_EOL;
+            }
+            //usleep(100000);
+        }
+    }
+}
+
---
+++ examples/redis-queue/producer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jenner
+ * Date: 2016/3/17
+ * Time: 19:53
+ */
+
+declare(ticks = 1);
+require dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+class Producer extends \Jenner\SimpleFork\Process {
+    public function run() {
+        $queue = new \Jenner\SimpleFork\Queue\RedisQueue('127.0.0.1', 6379);
+        for($i=0; $i<1000; $i++) {
+            $queue->put(getmypid() . '-' . mt_rand(0, 1000));
+        }
+        $queue->close();
+    }
+}
+
+$pool = new \Jenner\SimpleFork\Pool();
+for($i = 0; $i< 10; $i++) {
+    $process = new Producer();
+    $pool->execute($process);
+}
+
+$pool->wait(true);
